ebe6b30fd563de4cbbb0861b05a15b4e4f7ae06f
Gregory Ingraham <gingrah1@asu.edu>
2022-05-07 18:56:43 -0400

    This didn't work

---
+++ gp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Default message
+message="This didn't work"
+
+# Stage all passed arguments to commit
+git add "$@"
+
+# Prompt User for commit message
+echo "Please Enter Commit Message"
+read $message
+
+# Commit and Push
+git commit -m "$message" $@ 
+git push
